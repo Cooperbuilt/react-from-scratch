d72fd50c527836ecdaf4f2379f06c778a78df058
Evan Cooper <evcooper@wayfair.com>
2019-06-05 08:19:34 -0400

    Add babel dependencies and test file

--- render.js
+++ render.js
@@ -1,4 +1,20 @@
 /**
  * Render Methods 
- * 
  */
+
+
+/**
+* This renders an element to the dom
+* @method render
+* @param {object} element this is the element to render
+* @param {Object} parentDom A dom object
+* @return {void} executes a side effect
+*/
+
+export function render(element, parentDom) {
+  return 'hey';
+}
+
+
+
+
---
+++ tests/render.test.js
@@ -0,0 +1,13 @@
+/**
+ * Render Spec
+ *
+ * @author Evan Cooper 
+ */
+import {render} from '../render'
+
+describe('Render function', () => {
+  it('returns hey', () => {
+    expect(render()).toBe('hey')
+  });
+});
+
