2f971db0b059f92fad1bf90ae3e3446b87efb1d6
Evan Cooper <evcooper@wayfair.com>
2019-06-06 12:26:00 -0400

    Add support for props and listeners

--- render.js
+++ render.js
@@ -1,24 +1,47 @@
 /**
- * Render Methods 
+ * Render Methods
  */
 
-
 /**
-* This renders an element to the dom
-* @method render
-* @param {object} element this is the element to render
-* @param {Object} parentDom A dom object
-* @return {void} executes a side effect
-*/
+ * This renders an element to the dom
+ * @method render
+ * @param {object} element this is the element to render
+ * @param {Object} parentDom A dom object
+ * @return {void} executes a side effect
+ */
 
 export function render(element, parentDom) {
-  const {type, props} = element;
-  const dom = document.createElement(type)
+  const { type, props } = element;
+
+  // Create the DOM element
+  const isTextElement = type === "TEXT ELEMENT";
+  const dom = isTextElement
+    ? document.createTextNode("")
+    : document.createElement(type);
+
+  // Add event listeners
+  const isListener = name => name.startsWith("on");
+  Object.keys(props)
+    .filter(isListener)
+    .forEach(name => {
+      const eventType = name.toLowerCase().substring(2);
+      dom.addEventListener(eventType, props[name]);
+    });
+
+  // Set Props only on HTML elements and skip nodes
+  if (dom instanceof HTMLElement) {
+    const isAttribute = name => !isListener(name) && name != "children";
+    Object.keys(props)
+      .filter(isAttribute)
+      .forEach(name => {
+        dom.setAttribute(name, props[name]);
+      });
+  }
+
+  // Render children
   const childElements = props.children || [];
   childElements.forEach(childElement => render(childElement, dom));
+
+  // Append to parent
   parentDom.appendChild(dom);
 }
-
-
-
-
--- tests/render.test.js
+++ tests/render.test.js
@@ -8,25 +8,71 @@ import { render } from "../render";
 const ELEMENT = {
   type: "div",
   props: {
+    children: [{ type: "input", props: {} }]
+  }
+};
+
+const ELEMENT_WITH_PROPS = {
+  type: "div",
+  props: {
     id: "container",
     children: [
       { type: "input", props: { value: "foo", type: "text" } },
+      {
+        type: "a",
+        props: {
+          href: "/bar",
+          onClick: e => {
+            console.log(e);
+          }
+        }
+      },
+      {
+        type: "span",
+        props: {
+          children: [
+            {
+              type: "TEXT ELEMENT",
+              props: { nodeValue: "Foo" }
+            }
+          ]
+        }
+      }
+    ]
+  }
+};
+const ELEMENT_WITH_CUSTOM_PROPS = {
+  type: "div",
+  props: {
+    id: "container",
+    children: [
+      {
+        type: "input",
+        props: { "data-enzyme-id": "inputElement", type: "text" }
+      }
     ]
   }
 };
 
 describe("Render function", () => {
   beforeAll(() => {
-    Object.defineProperty(global, "document", {});
-  });
-  it("appends an element, including it's children, to the dom", () => {
-    var div = document.createElement("div");
+    let div = document.createElement("div");
     div.setAttribute("class", "root");
     document.body.appendChild(div);
-    const parentNode = document.querySelector('.root')
+  });
+
+  it("appends an element, including it's children, to the dom", () => {
+    const parentNode = document.querySelector(".root");
     render(ELEMENT, parentNode);
-    expect(document.querySelector("div").innerHTML).toEqual("<div><input></div>");
+    expect(document.querySelector("div").innerHTML).toEqual(
+      "<div><input></div>"
+    );
+  });
+  it("appends an element and adds props and listeners", () => {
+    const parentNode = document.querySelector(".root");
+    render(ELEMENT_WITH_PROPS, parentNode);
+    expect(document.querySelector("div").innerHTML).toBe(
+      '<div><input></div><div id=\"container\"><input value=\"foo\" type=\"text\"><a href=\"/bar\"></a><span></span></div>'
+    );
   });
 });
-
-//
