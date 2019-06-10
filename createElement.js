bca85d54fa14d86dff4a05b0caf069aa64a5b6d7
Evan Cooper <evcooper@wayfair.com>
2019-06-10 08:55:09 -0400

    Add JSDOM, working createElement, fix render test

---
+++ createElement.js
@@ -0,0 +1,26 @@
+/**
+* create element function
+*/
+
+/**
+* This creates an object representing a dom node,
+* @method render
+* @param {string} type this is the element type (i.e "div", "span" etc.)
+* @param {Object} props object representing all element props (i.e class, id, href, etc.)
+* @param {Object} TODO <= fix this
+* @return {object}
+*/
+
+export const createElement = (type, config, ...rest) => {
+  const props = Object.assign({}, config);
+  if (rest.length) {
+    props.children = rest
+    .filter(maybeChild => maybeChild != null && maybeChild !== false)
+    .map(child => child instanceof Object ? child : createTextElement(child));  
+  }
+  return { type, props };
+}
+
+const createTextElement = (value) => {
+  return createElement(TEXT_ELEMENT, { nodeValue: value });
+}
